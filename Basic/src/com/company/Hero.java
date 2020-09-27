0ccf6299bcbb9822f618ffa12470509c5ad8230d
guoying03 <2834168063@qq.com>
2020-09-27 11:58:16 +0800

    9.27

---
+++ Basic/src/com/company/Hero.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Hero {
+    String name;
+    float hp;
+    float armor;
+    int moveSpeed;
+
+    public static void main(String args[]){
+    Hero garen=new Hero();
+    garen.name="墨伦";
+    garen.hp=616.28f;
+    garen.armor=27.536f;
+    garen.moveSpeed=350;
+
+    Hero teemo=new Hero();
+    teemo.name="提莫";
+    teemo.hp=383f;
+    teemo.armor=14f;
+    teemo.moveSpeed=330;
+
+
+    }
+}
