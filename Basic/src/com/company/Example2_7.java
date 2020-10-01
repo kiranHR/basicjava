8c9642bd4f7d27afb0b0156472cf12a56ac78f52
guoying03 <2834168063@qq.com>
2020-10-01 13:16:01 +0800

    10.1

--- Basic/src/com/company/Example2_7.java
+++ Basic/src/com/company/Example2_7.java
@@ -1,2 +1,17 @@
-package com.company;public class Example2_7 {
+package com.company;
+
+import java.util.Arrays;
+
+public class Example2_7 {
+    public static void main(String args[]){
+        int[]a={12,34,9,23,45,6,45,90,123,19,34};
+        Arrays.sort(a);
+        System.out.println(Arrays.toString(a));
+        int number=45;
+        int index=Arrays.binarySearch(a,number);
+        if(index>=0){
+            System.out.println(number+"和数组中索引为"+index+"的元素的值相同");
+
+        }
+    }
 }
